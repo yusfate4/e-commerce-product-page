2c48928f7858bd8d45b2c4a71d8f6b4731040db7
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-21 00:00:49 +0100

    e-commerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import logo from "./images/logo.svg";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import avatar from "./images/image-avatar.png";
+
+function Header() {
+  return (
+    <>
+    <header className="flex items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
+    <div className="flex items-center justify-start gap-4">
+        <img src={logo} alt="" />
+
+        <nav>
+          <ul className="flex items-center justify-start gap-4">
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <ul className="flex items-center justify-start gap-4">
+          <li>
+            <button>
+              <AiOutlineShoppingCart />
+            </button>{" "}
+          </li>
+          <li>
+            <img src={avatar} alt="" className="w-12"/>{" "}
+          </li>
+        </ul>
+      </div>
+    </header>
+    
+    </>
+  );
+}
 
 function App() {
+  const [products] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <Header />
+    </>
   );
 }
 
